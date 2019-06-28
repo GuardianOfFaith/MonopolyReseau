8b5165ab6f4a0c52c1f9518d29a79924a01ad16f
DESKTOP-IQ1FH42\Esteban <esteban.dumas@free.fr>
2019-06-28 20:35:22 +0200

    plateau

---
+++ Assets/Propriete.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propriete : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string name;
+    public int id;
+    public int Thunes;
+
+    public enum TypeCase
+    {
+        Violet,
+        BleuClair,
+        Pourpre,
+        Orange,
+        Rouge,
+        Jaune,
+        Vert,
+        BleuFonce,
+        Gare,
+        Public,
+        Chance,
+        Communauté,
+        Parc,
+        Prison,
+        Taxe
+    };
+
+    public TypeCase Type;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
