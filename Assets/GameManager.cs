a2c9cc22af506484808262e58efbe821c25a1ba0
Angèle <nicolas.besso@aliceadsl.fr>
2019-07-24 23:53:09 +0200

    fix close active menu and repop endturn button

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -273,6 +273,13 @@ public class GameManager : MonoBehaviour
             }
             isRollingDice = false;
         }
+        else
+        {
+            foreach( GameObject g in guiButton)
+            {
+                g.SetActive(false);
+            }
+        }
     }
 
     
